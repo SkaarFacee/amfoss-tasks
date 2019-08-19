f0183fa11de22d42c9e18c9ffae3e7608fca5c68
adiaux <52825668+adiaux@users.noreply.github.com>
2019-08-20 02:46:33 +0530

    Add files via upload

---
+++ Task 6/asd.go
@@ -0,0 +1,28 @@
+package main
+
+import {"fmt"
+	"github.com/dghubble/go-twitter/twitter"
+    	"github.com/dghubble/oauth1"
+	"os"
+}
+func main(){
+config := oauth1.NewConfig("consumerKey", "consumerSecret")
+token := oauth1.NewToken("accessToken", "accessSecret")
+httpClient := config.Client(oauth1.NoContext, token)
+
+// Status Show
+tweet, resp, err := client.Statuses.Show(585613041028431872, nil)
+
+// Search Tweets
+search, resp, err := client.Search.Tweets(&twitter.SearchTweetParams{
+    Query: "gopher",
+})
+
+// User Show
+user, resp, err := client.Users.Show(&twitter.UserShowParams{
+    ScreenName: "dghubble",
+})
+
+// Followers
+followers, resp, err := client.Followers.List(&twitter.FollowerListParams{})
+}
